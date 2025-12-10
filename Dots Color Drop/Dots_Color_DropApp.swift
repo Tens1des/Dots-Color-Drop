1a514642e519c05e8e08b4174dd6dd8694082208
Tens1des <romankotov3213@gmail.com>
2025-12-10 16:17:55 +0300

    Initial Commit

---
+++ Dots Color Drop/Dots_Color_DropApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dots_Color_DropApp.swift
+//  Dots Color Drop
+//
+//  Created by Рома Котов on 10.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Dots_Color_DropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
